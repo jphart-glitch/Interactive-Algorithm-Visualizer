6026a69e1d742c5b35142e0c9eb1efb2d530bf71
John Hart <jphart@princeton.edu>
2022-10-25 15:19:28 -0400

    Array component made

--- app/static/js/my-app/src/array.js
+++ app/static/js/my-app/src/array.js
@@ -6,16 +6,44 @@
 'use strict';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value : props.value
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" draggable="true">
+        {this.state.value}
       </button>
     );
   }
+  // handleDragStart = event => {
+  //   // console.log("Drag of Square #" + this.props.value + " has begun!");
+  // }
+
+  // handleDragEnd = event => {
+  //   // console.log("Drag of Square #" + this.props.value + " has ended!");
+  // }
+
+  // handleDragEnter = event => {
+  //   // console.log("Dragging over Square #" + this.props.value + " has begun!");
+  //   event.stopPropagation();
+  //   event.preventDefault();
+  // }
+
+  // handleDragLeave = event => {
+  //   // console.log("Dragging over Square #" + this.props.value + " has ended!");
+  //   event.stopPropagation();
+  //   event.preventDefault();
+  // }
+  // onDragStart={this.handleDragStart} onDragEnd={this.handleDragEnd}
+  // onDragEnter={this.handleDragEnter} onDragLeave={this.handleDragLeave}
 }
 
-class Array extends React.Component {
+class FullArray extends React.Component {
   renderSquare(i) {
     return <Square value={i}/>;
   }
@@ -23,17 +51,26 @@ class Array extends React.Component {
   render() {
     return (
     <div>
-      <div>
-        {this.renderSquare(0)}
-      </div>
+      {this.renderSquare(0)}
+      {this.renderSquare(1)}
+      {this.renderSquare(2)}
+      {this.renderSquare(3)}
+      {this.renderSquare(4)}
+      {this.renderSquare(5)}
+      {this.renderSquare(6)}
+      {this.renderSquare(7)}
     </div>
     );
   }
 }
 
-class Game extends React.Component {
+class Simulation extends React.Component {
   render() {
-    return <Array />
+    return(
+    <div className="simulation">
+      <FullArray />
+    </div>
+    );
   }
 }
 
@@ -42,4 +79,4 @@ class Game extends React.Component {
 /* Load into container */
 const domContainer = document.querySelector('#array_container');
 const root = ReactDOM.createRoot(domContainer);
-root.render(<Array />);
+root.render(<Simulation />);
